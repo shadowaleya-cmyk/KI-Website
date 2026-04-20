2b4606c89e9932c5bc2a59ea103eaac2e87ab4b0
shadowaleya-cmyk <shadowaleya@gmail.com>
2026-04-20 09:45:40 +0000

    Add more organic biological elements: floating spheres, waves, additional blob shapes with fluid animations

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,31 @@ export default function App() {
     <LanguageProvider>
       <BrowserRouter>
         <CursorGlow />
+        {/* Main blob decorations */}
         <div className="blob-decoration blob-1"></div>
         <div className="blob-decoration blob-2"></div>
         <div className="blob-decoration blob-3"></div>
         <div className="blob-decoration blob-4"></div>
+        
+        {/* Additional organic floating elements */}
+        <div className="blob-decoration blob-5"></div>
+        <div className="blob-decoration blob-6"></div>
+        <div className="blob-decoration blob-7"></div>
+        
+        {/* Floating lines */}
+        <div className="organic-line organic-line-1"></div>
+        <div className="organic-line organic-line-2"></div>
+        <div className="organic-line organic-line-3"></div>
+        
+        {/* Floating spheres */}
+        <div className="floating-sphere sphere-1"></div>
+        <div className="floating-sphere sphere-2"></div>
+        <div className="floating-sphere sphere-3"></div>
+        
+        {/* Wave elements */}
+        <div className="wave-line wave-1"></div>
+        <div className="wave-line wave-2"></div>
+        
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
